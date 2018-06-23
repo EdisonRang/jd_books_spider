942c279064a46d40e63d87042d9bd1d14def9ae9
EdisonRang <rangjincheng@outlook.com>
2018-06-23 17:28:15 +0800

    提取首页数据

--- JD/JD/spiders/JD_Books.py
+++ JD/JD/spiders/JD_Books.py
@@ -14,4 +14,24 @@ class JdBooksSpider(scrapy.Spider):
         # 遍历列表获取信息
         for big in big_list:
             big_category = big.xpath('./text()').extract_first()
-            print(big_category)
+            big_category_link = "https:" + big.xpath('./@href').extract_first()
+            # 获取小分类节点列表
+            small_list = response.xpath('//*[@id="booksort"]/div[2]/dl/dd[1]/em/a')
+            # 遍历列表
+            for small in small_list:
+                small_category = small.xpath('./text()').extract_first()
+                small_category_link = "https:" + small.xpath('./@href').extract_first()
+                temp ={}
+                temp['big_category'] = big_category
+                temp['big_category_link'] = big_category_link
+                temp['small_category'] = small_category
+                temp['small_category_link'] = small_category_link
+                yield scrapy.Request(
+                    url=temp['small_category_link'],
+                    callback=self.parse_detail,
+                    meta={"meta_1":temp}
+                )
+
+    def parse_detail(self):
+        pass
+
